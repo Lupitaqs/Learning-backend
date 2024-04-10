d5d327ef9ec721744aef8381dad612ca17061a9f
Lupitaqs <lupitaqs@outlook.com>
2024-04-09 20:33:40 -0600

    Run the Express app on the node server

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res, next) => {
+    res.json({ message: 'Your request was successful!' });
+});
 
 module.exports = app;
+
