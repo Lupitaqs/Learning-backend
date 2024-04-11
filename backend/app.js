5c5212149a45186995a87214727ce920834272a8
Lupitaqs <lupitaqs@outlook.com>
2024-04-10 22:30:29 -0600

    Send back stuff for sale

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Request received!');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Your request was successful!' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Response sent successfully!');
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'qsdfqsdf'
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'qsdfqsdf'
+        }
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
+
+
