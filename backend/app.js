cb07418a971086f8f1ea1c60b6e456f0c77e0708
Lupitaqs <lupitaqs@outlook.com>
2024-04-10 23:28:08 -0600

    Set up MongoDB and connected the API to the MongoDB cluster

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
+// MONGODB PW: Lupe0158
+// MONGODB CONNECTION: mongodb+srv://mgqslupis:Lupe0158@cluster0.nivjftc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://mgqslupis:Lupe0158@cluster0.nivjftc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error);
+    });
 
 // Middleware to handle CORS errors
 app.use((req, res, next) => {
@@ -11,7 +24,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-
 
 // Middleware to handle GET requests
 app.use('/api/stuff', (req, res, next) => {
