69f2742dfd102df72c7ea322379557975ef2146b
Lupitaqs <lupitaqs@outlook.com>
2024-04-11 00:09:30 -0600

    Set up routing

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
 
@@ -27,101 +27,8 @@ app.use((req, res, next) => {
     next();
 });
 
-// Middleware to handle GET requests
-app.use('/api/stuff', (req, res, next) => {
-    Thing.find().then(
-        (things) => {
-            res.status(200).json(things);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle POST requests
-app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    thing.save()
-        .then(() => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error
-            });
-        });
-});
-
-// Middleware to retrieve an specific Thing
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-    }).then(
-        (thing) => {
-            res.status(200).json(thing);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle PUT requests
-app.put('/api/stuff/:id', (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    Thing.updateOne({ _id: req.params.id }, thing).then(
-        () => {
-            res.status(201).json({
-                message: 'Thing updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle DELETE requests
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+// Link the routes to the app
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
 
