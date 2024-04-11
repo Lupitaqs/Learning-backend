bbe2f6bc85eebe54d5080b57584ccf5f12474609
Lupitaqs <lupitaqs@outlook.com>
2024-04-10 23:37:57 -0600

    Retrieving an specific Thing

--- backend/app.js
+++ backend/app.js
@@ -64,6 +64,23 @@ app.post('/api/stuff', (req, res, next) => {
         });
 });
 
+// Middleware to retrieve an specific Thing
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({
+        _id: req.params.id
+    }).then(
+        (thing) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+});
+
 module.exports = app;
 
 
