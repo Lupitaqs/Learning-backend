b8486720f184fe69cdf2f889a2c9ed5f6c194a08
Lupitaqs <lupitaqs@outlook.com>
2024-04-10 23:43:22 -0600

    Deleting a Thing

--- backend/app.js
+++ backend/app.js
@@ -106,6 +106,23 @@ app.put('/api/stuff/:id', (req, res, next) => {
     );
 });
 
+// Middleware to handle DELETE requests
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
 module.exports = app;
 
 
