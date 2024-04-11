ab79f10d0f6c915003493a2acb2201d1ae22e4ff
Lupitaqs <lupitaqs@outlook.com>
2024-04-11 00:17:40 -0600

    Set up controllers

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,103 +1,13 @@
 const Express = require('express');
 const router = Express.Router();
 
-// Import the Thing model
-const Thing = require('./models/thing');
+// Controllers
+const stuffCtrl = require('../controllers/stuff');
 
-// Middleware to handle POST requests
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    thing.save()
-        .then(() => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error
-            });
-        });
-});
-
-// Middleware to retrieve an specific Thing
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-    }).then(
-        (thing) => {
-            res.status(200).json(thing);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle PUT requests
-router.put('/:id', (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    Thing.updateOne({ _id: req.params.id }, thing).then(
-        () => {
-            res.status(201).json({
-                message: 'Thing updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle DELETE requests
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
-
-// Middleware to handle GET requests
-router.get('/', (req, res, next) => {
-    Thing.find().then(
-        (things) => {
-            res.status(200).json(things);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
 
 module.exports = router;
