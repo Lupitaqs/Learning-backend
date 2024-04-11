0f6c5868c0b247379256cc5d4b839ac381ebe829
Lupitaqs <lupitaqs@outlook.com>
2024-04-11 00:41:17 -0600

    Create auth tokens

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
@@ -33,9 +34,13 @@ exports.login = (req, res, next) => {
                                     error: 'Incorrect password!' 
                                 });
                             }
+                            const token = jwt.sign(
+                                { userId: user._id },
+                                'RANDOM_TOKEN_SECRET',
+                                { expiresIn: '24h' };
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN'
+                                token: token
                             });
                         })
                     .catch(
