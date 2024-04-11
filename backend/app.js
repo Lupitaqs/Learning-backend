675f755116405412b6a427115091c25f49894b4d
Lupitaqs <lupitaqs@outlook.com>
2024-04-10 23:41:05 -0600

    Update a Thing

--- backend/app.js
+++ backend/app.js
@@ -81,6 +81,31 @@ app.get('/api/stuff/:id', (req, res, next) => {
     );
 });
 
+// Middleware to handle PUT requests
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    Thing.updateOne({ _id: req.params.id }, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
 module.exports = app;
 
 
